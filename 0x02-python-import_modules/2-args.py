aaf2c1d2c3d169776cdb96b8d366149e99dd2970
“name” <achrafassouam@gmail.com>
2024-01-04 13:29:49 -0800

    2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -4,9 +4,12 @@ if __name__ == "__main__":
 
     num_args = len(sys.argv)
 
-    if num_args > 1:
+    if (num_args - 1) == 1:
+        print("{} argument:".format(num_args - 1))
+        print("{}: {}".format((num_args - 1), sys.argv[1]))
+    elif num_args > 1:
         print("{} arguments:".format(num_args - 1))
         for i in range(1, num_args):
             print("{}: {}".format(i, sys.argv[i]))
     else:
-        print("{} argument.".format(num_args - 1))
+        print("{} arguments.".format(num_args - 1))
