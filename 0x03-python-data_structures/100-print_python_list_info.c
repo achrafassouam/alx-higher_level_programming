eef07b7c1b0efd9258059ae6ddc1524508d425a4
“name” <achrafassouam@gmail.com>
2024-01-08 15:18:37 -0800

    100-print_python_list_info.c

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -1,42 +1,29 @@
-#include <Python.h>
+#include "/usr/include/python3.8/Python.h"
+#include <stdio.h>
 
 /**
  * print_python_list_info - print python info
- * @p: PyObject 
- * Return: no return
+ * @p: pyobject
+ *
+ * Return: none.
  */
 
 void print_python_list_info(PyObject *p)
 {
-	PyListObject *list;
-	PyObject *item, *type_obj;
-	int list_len, allocated_memory, i;
-	char *type_name;
+	int i;
+	int list_len = 0;
+	PyObject *item;
+	PyListObject *clone = (PyListObject *) p;
 
-
-	if (!PyList_Check(p)) 
-	{
-		PyErr_SetString(PyExc_TypeError, "Object is not a Python list");
-		return;
-	}
-
-	list = (PyListObject *)p;
-
-	list_len = Py_SIZE(list);
-
+	list_len = Py_SIZE(p);
 	printf("[*] Size of the Python List = %d\n", list_len);
-
-	allocated_memory = (int)list->allocated;
-
-	printf("[*] Allocated = %d bytes\n", allocated_memory);
+	printf("[*] Allocated = %d\n", (int) clone->allocated);
 
 	for (i = 0; i < list_len; ++i)
 	{
-		item = PyList_GET_ITEM(list, i);
-		type_obj = item->ob_type;
-
-		type_name = type_obj->tp_name;
-
-		printf("Element %d: %s\n", i, type_name);
+		item = PyList_GET_ITEM(p, i);
+		printf("Element %d: %s\n", i, item->ob_type->tp_name);
 	}
+    return;
 }
+
