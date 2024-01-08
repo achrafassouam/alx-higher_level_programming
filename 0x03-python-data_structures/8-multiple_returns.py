32ac2f6e7c4b58c931f4d4bb01e7e6cb3ad80927
“name” <achrafassouam@gmail.com>
2024-01-08 13:47:50 -0800

    8-multiple_returns.py

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -2,7 +2,7 @@
 def multiple_returns(sentence):
     lenght = len(sentence)
     if lenght > 0:
-        first = sentence[0]
+        new = (lenght, sentence[0])
     else:
-        return None
-    return (lenght, first)
+        new = (lenght, None)
+    return new
