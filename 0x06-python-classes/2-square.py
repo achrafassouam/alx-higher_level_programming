cafb0005655b722d2263f65c6e0a5ecff0a943be
Achraf Assouam <achrafassouam@gmail.com>
2024-01-23 14:31:25 -0800

    fixing pycodestyle in 2-square.py

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -4,8 +4,6 @@
 
 class Square:
     """ Representing the class Square """
-    
-
     def __init__(self, size=0):
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
