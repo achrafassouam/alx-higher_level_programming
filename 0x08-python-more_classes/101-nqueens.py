7812de8394a3094ff05ab694bbd887278c0387bd
Achraf Assouam <achrafassouam@gmail.com>
2024-01-31 11:29:22 -0800

    101-nqueens.py

--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -14,31 +14,36 @@ except ValueError:
     sys.exit(1)
 
 if N < 4:
-    print("N must be at least 4") 
+    print("N must be at least 4")
     sys.exit(1)
+
 
 def solveNQueens(n):
     solutions = []
     solve(n, [], solutions)
     return solutions
 
+
 def solve(n, columnPlacement, solutions):
     if len(columnPlacement) == n:
         solutions.append(list(columnPlacement))
         return
-    
+
     for i in range(n):
         if isSafe(columnPlacement, i):
             columnPlacement.append(i)
             solve(n, columnPlacement, solutions)
             columnPlacement.pop()
-            
+
+
 def isSafe(columnPlacement, i):
     row = len(columnPlacement)
     for queen in range(row):
-        if columnPlacement[queen] == i or abs(columnPlacement[queen] - i) == row - queen:
+        diff = abs(columnPlacement[queen] - i)
+        if columnPlacement[queen] == i or diff == row - queen:
             return False
     return True
+
 
 for solution in solveNQueens(N):
     print([[i, solution[i]] for i in range(N)])
