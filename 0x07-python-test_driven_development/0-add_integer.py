e4bbf3b23eb26453d0b8703a382aea1528cc72fc
Achraf Assouam <achrafassouam@gmail.com>
2024-02-01 10:36:43 -0800

    adding try: and except: in 0-add_integer

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -13,12 +13,15 @@ def add_integer(a, b=98):
     a and b must be first casted to integers if they are float
     """
 
-    if not isinstance(a, (int, float)):
-        raise TypeError("a must be an integer")
-    if not isinstance(b, (int, float)):
-        raise TypeError("b must be an integer")
+    try:
+        if not isinstance(a, (int, float)):
+            raise TypeError("a must be an integer")
+        elif not isinstance(b, (int, float)):
+            raise TypeError("b must be an integer")
 
-    a = int(a)
-    b = int(b)
+        a = int(a)
+        b = int(b)
 
-    return a + b
+        return a + b
+    except Exception as e:
+        raise e
