5281302f48db4777d277e6c4d802265143e67b06
Achraf Assouam <achrafassouam@gmail.com>
2024-02-01 10:29:55 -0800

    adding 0-add_integer .py and .txt

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+function that adds 2 integers
+"""
+
+
+def add_integer(a, b=98):
+    """
+    Return a + b
+    if a and b aren't integer or float :
+        raise : TypeError
+        message : a or b must be an integer
+    a and b must be first casted to integers if they are float
+    """
+
+    if not isinstance(a, (int, float)):
+        raise TypeError("a must be an integer")
+    if not isinstance(b, (int, float)):
+        raise TypeError("b must be an integer")
+
+    a = int(a)
+    b = int(b)
+
+    return a + b
