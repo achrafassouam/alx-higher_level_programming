0f28c60f8543b34fabc2ecb1dae314192dfe0397
Achraf Assouam <achrafassouam@gmail.com>
2024-02-05 08:01:57 -0800

    0-lookup.py

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,3 +1,16 @@
 #!/usr/bin/python3
+"""
+    a functions that returns the list of available
+    attributes and methods of an object
+"""
+
+
+
 def lookup(obj):
+    """
+    Args: obj
+
+    Return: A list of strings, where each string
+        is the name of an attribute or method of the object.
+    """
     return dir(obj)
