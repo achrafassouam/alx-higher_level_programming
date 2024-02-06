9df862f90c03a8e5e6f4d004662c7fa9bd3c7050
Achraf Assouam <achrafassouam@gmail.com>
2024-02-06 11:44:25 -0800

    8-class_to_json.py

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -1,9 +1,14 @@
 #!/usr/bin/pyhon3
-""" d """
+"""
+a function that returns the dictionary description
+with simple data structure (list, dictionary, string, integer and boolean) 
+for JSON serialization of an object
+"""
+
 
 
 def class_to_json(obj):
-    """ d """
+    """ class to jason """
     return_dict = {}
     if hasattr(obj, "__dict__"):
         for key, value in obj.__dict__.items():
