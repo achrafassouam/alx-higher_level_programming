c0ff4f1d404047acb7c88df97f81e067e1f6877a
Achraf Assouam <achrafassouam@gmail.com>
2024-02-06 10:44:08 -0800

    4-from_json_string.py

---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" a function that returns an object (Python data structure)
+    represented by a JSON string
+"""
+
+import json
+
+
+def from_json_string(my_str):
+    """ return an object represented by a JSON string """
+    return json.loads(my_str)
