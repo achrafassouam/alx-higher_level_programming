f6685d33d8d4d5c7882467dedd3cceb85ef59eff
Achraf Assouam <achrafassouam@gmail.com>
2024-02-06 10:20:53 -0800

    1-write_file.py

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+    a function that writes a string to a text file (UTF8)
+    and returns the number of characters written:
+"""
+
+
+def write_file(filename="", text=""):
+    """ open and rewrite a file """
+    with open(filename, 'r', encoding="UTF8") as f:
+        f.write(text)
+        return len(text)
