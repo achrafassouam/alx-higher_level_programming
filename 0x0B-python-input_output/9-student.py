2cfd8a8a42c1e7e4c2052271995ccc90af791f7f
Achraf Assouam <achrafassouam@gmail.com>
2024-02-06 11:55:46 -0800

    9-student.py

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 """ class Student that defines a student by :
-        -first name;
-        -last name;
-        -age;
+        - first name;
+        - last name;
+        - age;
 """
 
 
@@ -11,6 +11,7 @@ class Student:
     def __init__(self, first_name, last_name, age):
         self.first_name = first_name
         self.last_name = last_name
+
         self.age = age
 
         def to_json(self):
