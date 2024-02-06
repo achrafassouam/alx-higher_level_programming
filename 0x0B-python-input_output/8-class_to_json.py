13f6089475621e783794bb2b0695b9ad34260df5
Achraf Assouam <achrafassouam@gmail.com>
2024-02-06 11:46:56 -0800

    8-class_to_json.py

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -9,13 +9,4 @@ for JSON serialization of an object
 
 def class_to_json(obj):
     """ class to jason """
-    return_dict = {}
-    if hasattr(obj, "__dict__"):
-        for key, value in obj.__dict__.items():
-            if type(value) in [list, dict, str, int, bool]:
-                return_dict[key] = value
-            elif type(value) is dict:
-                return_dict[key] = dict(value)
-            elif type(value) is list:
-                return_dict[key] = list(value)
-        return return_dict
+    return obj.__dict__
