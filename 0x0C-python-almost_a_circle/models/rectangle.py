f9b916dff2e0a9b252c194a12c724de9a56fa297
Achraf Assouam <achrafassouam@gmail.com>
2024-02-12 15:20:58 -0800

    rec

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -18,50 +18,63 @@ class Rectangle(Base):
 
     @property
     def width(self):
+        '''
+            return width
+        '''
         return self.__width
 
     @width.setter
     def width(self, value):
+        ''' set width '''
         self.setter_validation("width", value)
         self.__width = value
 
     @property
     def height(self):
+        ''' return height '''
         return self.__height
 
     @height.setter
     def height(self, value):
+        ''' set height '''
         self.setter_validation("height", value)
         self.__height = value
 
     @property
     def x(self):
+        ''' return x '''
         return self.__x
 
     @x.setter
     def x(self, value):
+        ''' set x '''
         self.setter_validation("x", value)
         self.__x = value
 
     @property
     def y(self):
+        ''' return y '''
         return self.__y
 
     @y.setter
     def y(self, value):
+        ''' set y '''
         self.setter_validation("y", value)
         self.__y = value
 
     def area(self):
+        ''' return area '''
         return (self.width * self.height)
 
     def display(self):
+        ''' print the rectangle '''
         print("\n" * self.y, end="")
         for i in range(self.height):
             print(" " * self.x, end="")
             print("#" * self.width)
 
     def __str__(self):
+        ''' updating str '''
         return (
             "[Rectangle] ({})".format(self.id) +
             " {}/{}".format(self.x, self.y) +
@@ -69,6 +82,7 @@ class Rectangle(Base):
         )
 
     def update(self, *args, **kwargs):
+        ''' updating the args '''
         if args:
             for i, arg in enumerate(args):
                 if i == 0:
@@ -96,6 +110,7 @@ class Rectangle(Base):
                     self.y = value
 
     def to_dictionary(self):
+        ''' returning a dictionaty of the square '''
         return {
                 'id': self.id,
                 'width': self.width,
@@ -105,6 +120,7 @@ class Rectangle(Base):
         }
 
     def setter_validation(self, name, value):
+        ''' returing the raised errors '''
         if type(value) != int:
             raise TypeError("{} must be an integer".format(name))
         if name in ["width", "height"] and value <= 0:
