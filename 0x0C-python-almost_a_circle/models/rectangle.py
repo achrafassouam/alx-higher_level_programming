cb7f443eaeae524aabf9c049cd1ee0ebea9613f7
Achraf Assouam <achrafassouam@gmail.com>
2024-02-12 14:31:27 -0800

    rectangle.py

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,17 +1,12 @@
 #!/usr/bin/python3
-'''
-    Class Rectangle
-'''
+""" Rectangle Class """
+
+
 from models.base import Base
 
 
 class Rectangle(Base):
-    '''
-        Defining the Rectangle class
-        Inherits from:
-            Base
-    '''
-
+    """ initialization of Rectangle class """
     def __init__(self, width, height, x=0, y=0, id=None):
         self.width = width
         self.height = height
@@ -21,120 +16,88 @@ class Rectangle(Base):
 
     @property
     def width(self):
-        '''
-            Returning private attribute
-        '''
         return self.__width
 
     @width.setter
     def width(self, value):
-        '''
-            Setting private attribute
-        '''
         self.setter_validation("width", value)
         self.__width = value
 
     @property
     def height(self):
-        '''
-            Returning private attribute
-        '''
         return self.__height
 
     @height.setter
     def height(self, value):
-        '''
-            Setting private attribute
-        '''
         self.setter_validation("height", value)
         self.__height = value
 
     @property
     def x(self):
-        '''
-            Returning private attribute
-        '''
         return self.__x
 
     @x.setter
     def x(self, value):
-        '''
-            Setting private attribute
-        '''
         self.setter_validation("x", value)
         self.__x = value
 
     @property
     def y(self):
-        '''
-            Returning private attribute
-        '''
         return self.__y
 
     @y.setter
     def y(self, value):
-        '''
-            Setting private attribute
-        '''
         self.setter_validation("y", value)
         self.__y = value
 
     def area(self):
-        '''
-            Returns the area of the rectangle
-        '''
-        return (self.height * self.width)
+        return (self.width * self.height)
 
     def display(self):
-        '''
-            Prints to stdout the representation of the rectangle
-        '''
-        rectangle = ""
         print("\n" * self.y, end="")
         for i in range(self.height):
-            rectangle += (" " * self.x) + ("#" * self.width) + "\n"
-        print(rectangle, end="")
-
-    def update(self, *args, **kwargs):
-        '''
-            Updates the arguments in the class
-        '''
-        if len(args) == 0:
-            for key, val in kwargs.items():
-                self.__setattr__(key, val)
-            return
-        try:
-            self.id = args[0]
-            self.width = args[1]
-            self.height = args[2]
-            self.x = args[3]
-            self.y = args[4]
-        except IndexError:
-            pass
-
-    def to_dictionary(self):
-        '''
-            Returns a dictionary representation of this class
-        '''
-        return {'x': getattr(self, "x"),
-                'y': getattr(self, "y"),
-                'id': getattr(self, "id"),
-                'height': getattr(self, "height"),
-                'width': getattr(self, "width")}
-
-    @staticmethod
-    def setter_validation(attribute, value):
-        if type(value) != int:
-            raise TypeError("{} must be an integer".format(attribute))
-        if attribute == "x" or attribute == "y":
-            if value < 0:
-                raise ValueError("{} must be >= 0".format(attribute))
-        elif value <= 0:
-            raise ValueError("{} must be > 0".format(attribute))
+            print(" " * self.x, end="")
+            print("#" * self.width)
 
     def __str__(self):
-        '''
-            Overwritting the str method
-        '''
-        return "[Rectangle] ({}) {}/{} - {}/{}".format(self.id, self.x, self.y,
-                                                       self.width, self.height)
+        return (
+            "[Rectangle] ({})".format(self.id) +
+            " {}/{}".format(self.x, self.y) +
+            " - {}/{}".format(self.width, self.height)
+        )
+
+    def update(self, *args, **kwargs):
+        if args:
+            for i, arg in enumerate(args):
+                if i == 0:
+                    self.id = arg
+                elif i == 1:
+                    self.width = arg
+                elif i == 2:
+                    self.height = arg
+                elif i == 3:
+                    self.x = arg
+                elif i == 4:
+                    self.y = arg
+
+        elif kwargs:
+            for key, value in kwargs.items():
+                if key == "id":
+                    self.id = value
+                elif key == "width":
+                    self.width = value
+                elif key == "height":
+                    self.height = value
+                elif key == "x":
+                    self.x = value
+                elif key == "y":
+                    self.y = value
+
+    def to_dictionary(self):
+        return {
+                'id': self.id,
+                'width': self.width,
+                'height': self.height,
+                'x': self.x,
+                'y': self.y
+        }
