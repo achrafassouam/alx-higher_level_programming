b7cfbe6e885ceeebb4e165340b7aeffef5532748
Achraf Assouam <achrafassouam@gmail.com>
2024-02-12 14:55:48 -0800

    rec

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
-"""
+'''
     Rectangle Class
-"""
+'''
 from models.base import Base
 
 
 class Rectangle(Base):
-    """
+    '''
         initialization of Rectangle class
-    """
+    '''
     def __init__(self, width, height, x=0, y=0, id=None):
         self.width = width
         self.height = height
