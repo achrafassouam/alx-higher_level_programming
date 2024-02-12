1d4949e91ee238f4ab6b38a6964d1945af8bec21
Achraf Assouam <achrafassouam@gmail.com>
2024-02-12 15:28:07 -0800

    adding docs to square.py

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3
-""" Square Class that inherits from Rectangle """
-
-
+'''
+    Square Class that inherits from Rectangle
+'''
 from models.rectangle import Rectangle
 
 
 class Square(Rectangle):
-    """ initialization of square class """
+    '''
+        initialization of square class
+    '''
     def __init__(self, size, x=0, y=0, id=None):
         super().__init__(size, size, x, y, id)
 
