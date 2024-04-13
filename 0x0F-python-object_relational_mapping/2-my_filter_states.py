d2eb98cd434b02da4382a51c11c4fd31a850864a
“Achraf <achrafassouam@gmail.com>
2024-04-13 16:29:31 -0700

    2-my_filter_states.py

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+""" script that lists all states from the database hbtn_0e_0_usa """
+
+import MySQLdb
+from sys import argv
+
+""" code should not be executed when imported """
+if __name__ == '__main__':
+
+    db = MySQLdb.connect(
+            host="localhost",
+            port=3306,
+            user=argv[1],
+            passwd=argv[2],
+            db=argv[3]
+    )
+
+    mycursor = db.cursor()
+
+    mycursor.execute("SELECT * FROM states WHERE name LIKE BINARY\
+                    '{}'".format(argv[4]))
+
+    rows = mycursor.fetchall()
+    for i in rows:
+        print(i)
+
+    mycursor.close()
+    db.close()
