18092e0c4e0ef43792fa609785d06ba2956710dc
“Achraf <achrafassouam@gmail.com>
2024-04-14 10:08:06 -0700

    7-model_state_fetch_all.py

---
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+    a script that lists all State objects from the database hbtn_0e_6_usa
+"""
+
+if __name__ == "__main__":
+    from sys import argv
+    from model_state import Base, State
+    from sqlalchemy.orm import sessionmaker
+    from sqlalchemy import create_engine
+
+    if __name__ == "__main__":
+        """
+        get states from database
+        """
+
+        db_uri = 'mysql+mysqldb://{}:{}@localhost:3306/{}'\
+            .format(
+                    argv[1],
+                    argv[2],
+                    argv[3]
+            )
+        engine = create_engine(db_uri)
+        Session = sessionmaker(bind=engine)
+
+        session = Session()
+
+        for instance in session.query(State).order_by(State.id):
+            print('{0}: {1}'.format(instance.id, instance.name))
