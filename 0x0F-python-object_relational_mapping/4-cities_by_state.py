96ee10af54805fa3139830d08aeedccfabfb7b7b
“Achraf <achrafassouam@gmail.com>
2024-04-14 06:05:57 -0700

    4-cities_by_state.

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+""" write a script that takes in arguments and displays all values in states
+    table of hbtn_0e_0_usa where name matches the argument.
+    But this time, write one that is safe from MySQL injections
+"""
+
+import MySQLdb
+from sys import argv
+
+""" code should not be executed when imported """
+if __name__ == '__main__':
+
+    db = MySQLdb.connect(
+            host="localhost",
+            port=3306,
+            user=argv[1],
+            passwd=argv[2],
+            db=argv[3]
+    )
+
+    mycursor = db.cursor()
+
+    sql = """
+        SELECT
+            cities.id, cities.name, states.name
+        FROM
+            cities
+        INNER JOIN
+            states
+        ON
+            cities.state_id = states.id
+        ORDER BY
+            cities.id
+    """
+    mycursor.execute(sql)
+
+    rows = mycursor.fetchall()
+    for i in rows:
+        print(i)
+
+    mycursor.close()
+    db.close()
