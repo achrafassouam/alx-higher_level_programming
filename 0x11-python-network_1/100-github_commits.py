9be47df7e64ae9ccb99c489c0c935639d0d286e3
“Achraf <achrafassouam@gmail.com>
2024-04-26 13:19:15 -0700

    updating 100-github_commits.py

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -7,19 +7,19 @@ and uses the GitHub API to display your id
 from sys import argv
 import requests
 
-
 if __name__ == "__main__":
     repo = argv[1]
     owner = argv[2]
     url = "https://api.github.com/repos/{}/{}/commits".format(repo, owner)
 
     response = requests.get(url)
-    commits = response.json()
 
-    try:
-        for commit in range(10):
-            print("{}: {}".format(
-                commits[commit].get("sha"),
-                commits[commit].get("commit").get("author").get("name")))
-    except IndexError:
-        pass
+    if response.status_code == 200:
+        commits = response.json()
+        for commit in commits[:10]:
+            sha = commit.get('sha')
+            author_name = commit.get('commit').get('author').get('name')
+            print("{}: {}".format(sha, author_name))
+    else:
+        print("Error fetching commits.")
+        print("Status code: {}".format(response.status_code))
