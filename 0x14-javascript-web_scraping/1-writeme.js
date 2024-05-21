13c361716808a234f865f679c09e502c130f11bc
“Achraf <achrafassouam@gmail.com>
2024-05-21 20:38:50 +0100

    updating 1-writteme.js

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -2,7 +2,7 @@
 
 /*  script that writes a string to a file of a file
     first argument is the file path
-	second argument is the string to write
+    second argument is the string to write
     The content of the file must be read in utf-8
     If an error occurred during the reading, print the error object
 */
@@ -11,7 +11,6 @@ const fs = require('fs');
 
 fs.writeFile(process.argv[2], process.argv[3], 'utf-8', (err) => {
   if (err) {
-	console.error(err);
+    console.error(err);
   }
 });
-
