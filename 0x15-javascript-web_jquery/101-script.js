328e3cb9fbf0ece60e23d5188b471f5a708d037b
“Achraf <achrafassouam@gmail.com>
2024-05-29 19:50:08 +0100

    documentation for 101-script.js

--- 0x15-javascript-web_jquery/101-script.js
+++ 0x15-javascript-web_jquery/101-script.js
@@ -1,3 +1,8 @@
+/* A JS script that adds, removes and clears LI elements
+ * from a list when the user clicks
+ * must use the JQuery API
+ */
+
 $(document).ready(function () {
   $('#add_item').click(function () {
     $('ul.my_list').append('<li>Item</li>');
