2babbd3b8e538b76559676c7b3f82630ea3d0161
“Achraf <achrafassouam@gmail.com>
2024-05-29 12:50:18 +0100

    3-script.js

---
+++ 0x15-javascript-web_jquery/3-script.js
@@ -0,0 +1,7 @@
+/* A JS script that adds the class red to the <header> element
+   when the user clicks on the tag DIV#red_header
+   must use the JQuery API
+*/
+$('div#red_header').click(function () {
+  $('header').addClass('red');
+});
