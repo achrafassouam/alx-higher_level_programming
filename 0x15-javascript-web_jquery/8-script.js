50980b8165d6f6397371455a876e62373d30ced9
“Achraf <achrafassouam@gmail.com>
2024-05-29 15:34:17 +0100

    8-script.js

---
+++ 0x15-javascript-web_jquery/8-script.js
@@ -0,0 +1,10 @@
+/* A JS script that fetches and lists the title for all movies from a URL
+*  must be displayed in the HTML tag UL#list_movies
+*  must use the JQuery API
+*/
+
+$.getJSON('https://swapi-api.alx-tools.com/api/films/?format=json', function (data) {
+  $.each(data.results, function (i, movie) {
+    $('ul#list_movies').append('<li>' + movie.title + '</li>');
+  });
+});
