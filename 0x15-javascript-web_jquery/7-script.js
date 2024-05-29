8a878b80ca9ffa73e497c751d9a15b743da70fb6
“Achraf <achrafassouam@gmail.com>
2024-05-29 15:30:22 +0100

    7-script.js

---
+++ 0x15-javascript-web_jquery/7-script.js
@@ -0,0 +1,8 @@
+/* A JS script that fetches the character name from a URL
+*  must be displayed in the HTML tag DIV#character
+*  must use the JQuery API
+*/
+
+$.getJSON('https://swapi-api.alx-tools.com/api/people/5/?format=json', function (data) {
+  $('div#character').text(data.name);
+});
